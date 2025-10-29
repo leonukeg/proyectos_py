0e70b123b9a862bc6170e9e75d043c80ab994380
leonukeg <chemaruan@gmail.com>
2025-10-29 07:41:00 +0100

    Implementa función spin en slot.py

--- slot_machine/src/slot.py
+++ slot_machine/src/slot.py
@@ -1,6 +1,9 @@
+import random
+
 def spin():
     """Gira los tres carretes y devuelve una lista con los resultados"""
-    pass
+    symbolos = ["🍒", "🍋", "🍉", "⭐", "🧡", "7️⃣", "💎"]
+    return [random.choice(symbolos) for _ in range(3 )]   
 
 
 def calculate_prize(resultado):
